e615e2c32d0b8e24d8ab24688959cde6108a99bf
Rahulkumar Varma <varmarahul2200@gmail.com>
2024-09-12 22:44:05 +0530

    Update TestRunner.java

--- src/test/java/MyRunner/TestRunner.java
+++ src/test/java/MyRunner/TestRunner.java
@@ -9,7 +9,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/resources/features",
+		features = "src/test/resources/Features",
 		glue = "StepDefinitions",
 		tags="@BankManager or @Customer",
 		plugin = { "pretty",				
