81d9eaa9f814eb7e1f0b09000f63e586e29f2943
Rahulkumar Varma <varmarahul2200@gmail.com>
2024-09-12 22:43:49 +0530

    Update TestNGTestRunner.java

--- src/test/java/MyRunner/TestNGTestRunner.java
+++ src/test/java/MyRunner/TestNGTestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		features = "src/test/resources/features",
+		features = "src/test/resources/Features",
 		glue = "StepDefinitions",
 		tags="@BankManager",
 		plugin = { "pretty", "json:target/cucumber-reports/json-report.json",
